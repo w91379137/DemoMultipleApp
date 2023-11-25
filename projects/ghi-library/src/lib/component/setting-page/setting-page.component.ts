87c1df260440d26efdd524bb533a9b18fe6a77aa
w91379137 <w913791379137@gmail.com>
2023-11-26 05:01:49 +0800

    ng g c component/SettingPage --project=ghi-library ng g c component/SettingPage --project=ProjectABC ng g c component/SettingPage --project=ProjectDEF

---
+++ projects/ghi-library/src/lib/component/setting-page/setting-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ghi-setting-page',
+  templateUrl: './setting-page.component.html',
+  styleUrls: ['./setting-page.component.css']
+})
+export class SettingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/project-def/src/app/app.module.ts
+++ projects/project-def/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GhiLibraryModule } from 'projects/ghi-library/src/lib/ghi-library.module';
+import { SettingPageComponent } from './component/setting-page/setting-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingPageComponent
   ],
   imports: [
     BrowserModule,
