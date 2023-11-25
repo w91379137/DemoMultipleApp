dc380b7ea6c7d0b45c1eb745f2c06aab30ab5383
w91379137 <w913791379137@gmail.com>
2023-11-26 05:03:14 +0800

    ng g c component/SettingItem --project=ghi-library

---
+++ projects/ghi-library/src/lib/component/setting-item/setting-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ghi-setting-item',
+  templateUrl: './setting-item.component.html',
+  styleUrls: ['./setting-item.component.css']
+})
+export class SettingItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
