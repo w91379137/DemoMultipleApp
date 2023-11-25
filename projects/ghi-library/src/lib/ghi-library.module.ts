03954705de2d28458cf82bc00a850b1d562f972a
w91379137 <w913791379137@gmail.com>
2023-11-26 04:45:04 +0800

    ng g c component/HomeItem --project=ghi-library

--- projects/ghi-library/src/lib/ghi-library.module.ts
+++ projects/ghi-library/src/lib/ghi-library.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { GhiLibraryComponent } from './ghi-library.component';
 import { HomePageComponent } from './component/home-page/home-page.component';
+import { HomeItemComponent } from './component/home-item/home-item.component';
 
 let components = [
   GhiLibraryComponent,
-  HomePageComponent
+  HomePageComponent,
+  HomeItemComponent
 ]
 
 @NgModule({
--- projects/ghi-library/src/public-api.ts
+++ projects/ghi-library/src/public-api.ts
@@ -8,6 +8,7 @@ export * from './lib/ghi-library.service';
 // component
 export * from './lib/ghi-library.component';
 export * from './lib/component/home-page/home-page.component';
+export * from './lib/component/home-item/home-item.component';
 
 // module
 export * from './lib/ghi-library.module';
